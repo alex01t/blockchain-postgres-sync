c9483e949734e45f3ff3e3dca0c5dd452af5baca
Аleksej Petrov <apetrov@wavesplatform.com>
2022-01-18 21:08:40 +0300

    fixed blocks spliting, quotes recognition

--- src/api/requestBlocksBatch.js
+++ src/api/requestBlocksBatch.js
@@ -1,5 +1,5 @@
 const request = require("superagent");
-require('superagent-retry-delay')(request);
+require("superagent-retry-delay")(request);
 const { USER_AGENT } = require("./constants");
 
 function unfold(fn, seed) {
@@ -14,26 +14,31 @@ function unfold(fn, seed) {
 
 // split {blockData},{blockData},{blockData} to array of {blockData}
 // blockData may contain symbols {}, so its need to count
-const splitBlocks = s =>
-  unfold(cur => {
+const splitBlocks = (s) =>
+  unfold((cur) => {
     // position after last close curve bracket
     let end = -1;
     // counter of curve brackets
     let c = 1;
     // current position
     let i = 0;
-    // whether current processing part of s is quoted
-    let quoted = 0;
-    // whether block found 
+    // whether current processing part of cur is quoted
+    let quoted = false;
+    // whether block found
     let found = false;
     while (i < cur.length && !found) {
       if (cur[i] === '"') {
-        // whether it not escaped quote
-        if (i == 0 || cur[i-1] !== '\\') {
+        let isEscapedQuote = false;
+        // escaped quote, but not escaped slash
+        if (i > 1 && cur[i - 1] == "\\" && cur[i - 2] !== "\\") {
+          isEscapedQuote = true;
+        }
+        // whether it is not escaped quote (syntax quote, but not in the text)
+        if (i == 0 || !isEscapedQuote) {
           quoted = !quoted;
         }
       }
-      
+
       // it doesn't need to handle quoted text
       if (quoted) {
         i++;
@@ -41,6 +46,7 @@ const splitBlocks = s =>
       } else {
         if (cur[i] === "{") c++;
         else if (cur[i] === "}") c--;
+
         if (c === 1) {
           end = i;
           found = true;
@@ -58,28 +64,28 @@ const splitBlocks = s =>
     }
   }, s);
 
-const parseBlocks = sanitize => (res, fn) => {
+const parseBlocks = (sanitize) => (res, fn) => {
   res.text = "";
   res.setEncoding("utf8");
-  res.on("data", chunk => (res.text += chunk));
-  res.on("end", err => fn(err, splitBlocks(sanitize(res.text).slice(1, -1))));
+  res.on("data", (chunk) => (res.text += chunk));
+  res.on("end", (err) => fn(err, splitBlocks(sanitize(res.text).slice(1, -1))));
 };
 
 // \u0000 in JSON is problematic for PostgreSQL
 // removing it from strings
-const sanitize = text => text.replace(/\\u0000/g, "");
+const sanitize = (text) => text.replace(/\\u0000/g, "");
 
 const requestBlocksBatch = (start, options) =>
   request
     .get(
-      `${options.nodeAddress}/blocks/seq/${start}/${start +
-        options.blocksPerRequest -
-        1}`
+      `${options.nodeAddress}/blocks/seq/${start}/${
+        start + options.blocksPerRequest - 1
+      }`
     )
     .set("User-Agent", USER_AGENT)
     .retry(options.nodePollingRetriesCount, options.nodePollingRetriesDelay)
     .buffer(true)
     .parse(parseBlocks(sanitize))
-    .then(r => r.body);
+    .then((r) => r.body);
 
 module.exports = requestBlocksBatch;
